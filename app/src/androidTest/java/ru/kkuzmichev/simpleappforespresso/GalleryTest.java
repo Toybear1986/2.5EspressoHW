f7eff2a767c50069f75b6d7d903e33473874e74c
Василий Скоробогатов <vaskorobogatov@yandex.ru>
2023-06-07 03:38:44 +0300

    Idling Resources Add utility class EspressoIdlingResources.java Add increment and decrement to GalleryFragment.java Add GalleryTest.java with idling recourses Add GalleryTest Report

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/GalleryTest.java
@@ -0,0 +1,56 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class GalleryTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityTestRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void registerIdlingResources() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+    }
+
+    @Test
+    public void galleryTest() {
+        ViewInteraction menu =
+                onView(withContentDescription("Open navigation drawer"));
+        menu.check(matches(isDisplayed()));
+        menu.perform(click());
+
+        ViewInteraction gallery = onView(withId(R.id.nav_gallery));
+        gallery.check(matches(isDisplayed()));
+        gallery.perform(click());
+        ViewInteraction galleryItem = onView(allOf(withId(R.id.item_number), withText("7")));
+        galleryItem.check(matches(withText("7")));
+    }
+
+    @After
+    public void unregisterIdlingResources() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+    }
+}
